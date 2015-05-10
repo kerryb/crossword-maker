ec9ef97f8da818c2b28e61ad9599cc47f1b42bdd
Kerry Buckley <kerryjbuckley@gmail.com>
2015-05-10 11:02:37 +0100

    Reset grid to (one choice of) base pattern

--- js/grid.js
+++ js/grid.js
@@ -1,13 +1,24 @@
 $(function() {
   createGrid();
+  resetGrid();
 });
 
-function createGrid(index) {
+function createGrid() {
   _.each(_.range(15), function(row) {
     $("#grid").append('<tr class="row-' + row + '"></tr>');
     row = $(".row-" + row)
     _.each(_.range(15), function(col) {
       row.append('<td class="col-' + col + '"></td>');
+    });
+  });
+}
+
+function resetGrid() {
+  $("#grid td").removeClass("block");
+  _.each(_.range(1, 14, 2), function(row) {
+    _.each(_.range(1, 14, 2), function(col) {
+      console.log(row, col);
+      $("#grid .row-" + row + " .col-" + col).addClass("block");
     });
   });
 }
